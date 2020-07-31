3e9d01612cafe1a8e6f9b53cd49a091a89fdd0ed
Alwin Lazar <alwin@zaconic.com>
2020-07-31 19:36:25 +0530

    Initial Commit

---
+++ lib/profile_page.dart
@@ -0,0 +1,284 @@
+import 'package:flutter/material.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
+
+class ProfilePage extends StatefulWidget {
+  @override
+  _ProfilePageState createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  bool _isOpen = false;
+  PanelController _panelController = PanelController();
+  var _imageList = [
+    'images/1.jpg',
+    'images/2.jpeg',
+    'images/3.jpg',
+    'images/4.jpeg',
+    'images/5.jpg',
+    'images/6.jpg',
+    'images/7.jpeg',
+    'images/8.jpg',
+    'images/9.jpg',
+    'images/10.jpeg',
+    'images/11.png',
+    'images/12.jpeg',
+    'images/13.jpg',
+    'images/14.jpg',
+    'images/15.jpg',
+    'images/16.jpeg',
+    'images/17.jpg',
+    'images/18.jpeg',
+  ];
+
+  /// **********************************************
+  /// LIFE CYCLE METHODS
+  /// **********************************************
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          FractionallySizedBox(
+            alignment: Alignment.topCenter,
+            heightFactor: 0.7,
+            child: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('images/profile.jpg'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+
+          FractionallySizedBox(
+            alignment: Alignment.bottomCenter,
+            heightFactor: 0.3,
+            child: Container(
+              color: Colors.white,
+            ),
+          ),
+
+          /// Sliding Panel
+          SlidingUpPanel(
+            controller: _panelController,
+            borderRadius: BorderRadius.only(
+              topRight: Radius.circular(32),
+              topLeft: Radius.circular(32),
+            ),
+            minHeight: MediaQuery.of(context).size.height * 0.35,
+            maxHeight: MediaQuery.of(context).size.height * 0.85,
+            body: GestureDetector(
+              onTap: () => _panelController.close(),
+              child: Container(
+                color: Colors.transparent,
+              ),
+            ),
+            panelBuilder: (ScrollController controller) =>
+                _panelBody(controller),
+            onPanelSlide: (value) {
+              if (value >= 0.2) {
+                if (!_isOpen) {
+                  setState(() {
+                    _isOpen = true;
+                  });
+                }
+              }
+            },
+            onPanelClosed: () {
+              setState(() {
+                _isOpen = false;
+              });
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  /// **********************************************
+  /// WIDGETS
+  /// **********************************************
+
+  /// Panel Body
+  SingleChildScrollView _panelBody(ScrollController controller) {
+    double hPadding = 40;
+
+    return SingleChildScrollView(
+      controller: controller,
+      physics: ClampingScrollPhysics(),
+      child: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: hPadding),
+            height: MediaQuery.of(context).size.height * 0.35,
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                _titleSection(),
+                _infoSection(),
+                _actionSection(hPadding: hPadding),
+              ],
+            ),
+          ),
+          GridView.builder(
+            primary: false,
+            shrinkWrap: true,
+            padding: EdgeInsets.zero,
+            itemCount: _imageList.length,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
+              mainAxisSpacing: 16,
+            ),
+            itemBuilder: (BuildContext context, int index) => Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(_imageList[index]),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  /// Action Section
+  Row _actionSection({double hPadding}) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Visibility(
+          visible: !_isOpen,
+          child: Expanded(
+            child: OutlineButton(
+              onPressed: () => _panelController.open(),
+              borderSide: BorderSide(color: Colors.blue),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30)),
+              child: Text(
+                'VIEW PROFILE',
+                style: TextStyle(
+                  fontFamily: 'NimbusSanL',
+                  fontSize: 12,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Visibility(
+          visible: !_isOpen,
+          child: SizedBox(
+            width: 16,
+          ),
+        ),
+        Expanded(
+          child: Container(
+            alignment: Alignment.center,
+            child: SizedBox(
+              width: _isOpen
+                  ? (MediaQuery.of(context).size.width - (2 * hPadding)) / 1.6
+                  : double.infinity,
+              child: FlatButton(
+                onPressed: () => print('Message tapped'),
+                color: Colors.blue,
+                textColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30)),
+                child: Text(
+                  'MESSAGE',
+                  style: TextStyle(
+                    fontFamily: 'NimbusSanL',
+                    fontSize: 12,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  /// Info Section
+  Row _infoSection() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        _infoCell(title: 'Projects', value: '1135'),
+        Container(
+          width: 1,
+          height: 40,
+          color: Colors.grey,
+        ),
+        _infoCell(title: 'Hourly Rate', value: "\$65"),
+        Container(
+          width: 1,
+          height: 40,
+          color: Colors.grey,
+        ),
+        _infoCell(title: 'Location', value: 'Dusseldorf'),
+      ],
+    );
+  }
+
+  /// Info Cell
+  Column _infoCell({String title, String value}) {
+    return Column(
+      children: <Widget>[
+        Text(
+          title,
+          style: TextStyle(
+            fontFamily: 'OpenSans',
+            fontWeight: FontWeight.w300,
+            fontSize: 14,
+          ),
+        ),
+        SizedBox(
+          height: 8,
+        ),
+        Text(
+          value,
+          style: TextStyle(
+            fontFamily: 'OpenSans',
+            fontWeight: FontWeight.w700,
+            fontSize: 14,
+          ),
+        ),
+      ],
+    );
+  }
+
+  /// Title Section
+  Column _titleSection() {
+    return Column(
+      children: <Widget>[
+        Text(
+          'Aanika Johnson',
+          style: TextStyle(
+            fontFamily: 'NimbusSanL',
+            fontWeight: FontWeight.w700,
+            fontSize: 30,
+          ),
+        ),
+        SizedBox(
+          height: 8,
+        ),
+        Text(
+          'Freelancer',
+          style: TextStyle(
+            fontFamily: 'NimbusSanL',
+            fontStyle: FontStyle.italic,
+            fontSize: 16,
+          ),
+        ),
+      ],
+    );
+  }
+}
